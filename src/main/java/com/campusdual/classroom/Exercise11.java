4a5bdbd9da03b888270442fddac0953c7e9ec9ec
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-09-26 11:18:48 +0200

    Terminado el ejercicio 11

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,16 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
-    }
 
+        System.out.println(blackRemote.getVolume());
+        blackRemote.volumeUp();
+        System.out.println(blackRemote.getVolume());
+
+        System.out.println(blackRemote.getChannel());
+
+        System.out.println(blackRemote.turnOn());
+        System.out.println(blackRemote.isOn());
+        System.out.println(blackRemote.turnOff());
+
+    }
 }
